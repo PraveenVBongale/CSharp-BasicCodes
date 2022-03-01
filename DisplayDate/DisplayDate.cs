741836a75d9f3d35c38df580c6ea4fd945b7f80c
PraveenVBongale <praveen.bongale98@gmail.com>
2022-03-01 13:22:40 +0530

    Date&Year

---
+++ DisplayDate/DisplayDate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisplayDate
+{
+    internal class DisplayDate
+    {
+        static int Main()
+        {
+            DateTime date = new DateTime(2022, 2, 25);
+            Console.WriteLine("Some Date Formats : ");
+            Console.WriteLine("Date and Time:  {0}", date);
+            Console.WriteLine(date.ToString("yyyy-MM-dd"));
+            Console.WriteLine(date.ToString("dd-MMM-yy"));
+            Console.WriteLine(date.ToString("M/d/yyyy"));
+            Console.WriteLine(date.ToString("M/d/yy"));
+            Console.WriteLine(date.ToString("MM/dd/yyyy"));
+            Console.WriteLine(date.ToString("MM/dd/yy"));
+            Console.WriteLine(date.ToString("yy/MM/dd"));
+            Console.Read();
+            return 0;
+        }
+    }
+}
