fc1cba1addd241c31cac30b14940002dd73cbbf5
PraveenVBongale <praveen.bongale98@gmail.com>
2022-03-01 13:27:50 +0530

    Bitwise_&_Swapping

---
+++ PersonHeight/Heights.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonHeight
+{
+    internal class Heights
+    {
+        public static void Main()
+        {
+            float height;
+            Console.WriteLine("Enter  the Height (in centimeters) \n");
+            height = int.Parse(Console.ReadLine());
+            if (height < 150.0)
+                Console.WriteLine("Dwarf \n");
+            else if ((height >= 150.0) && (height <= 165.0))
+                Console.WriteLine(" Average Height \n");
+            else if ((height >= 165.0) && (height <= 195.0))
+                Console.WriteLine("Taller \n");
+            else
+                Console.WriteLine("Abnormal height \n");
+        }
+    }
+}
