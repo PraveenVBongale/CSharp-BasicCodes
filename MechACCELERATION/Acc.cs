dca143d7f9229ae02c36ff5f79fa5ce488dd91bc
PraveenVBongale <praveen.bongale98@gmail.com>
2022-03-01 13:48:03 +0530

    Interface and Looping Operations

---
+++ MechACCELERATION/Acc.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MechACCELERATION
+{
+    internal class Acc
+    {
+        static void Main(string[] args)
+        {
+            int v, t, acc;
+            Console.WriteLine("---WELCOME TO CALCULATE Acc in (m/s^2) ---");
+            Console.WriteLine("Enter the Velocity 'v' in m/s = ");
+            v = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Time 't' in sec = ");
+            t = int.Parse(Console.ReadLine());
+            acc = v / t;
+            Console.WriteLine("Acceleration 'A'= {0} m/s^2", acc);
+        }
+    }
+}
