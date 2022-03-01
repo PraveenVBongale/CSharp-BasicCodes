85e4f381d4d0a236f8ebb310c91dd1202427d79e
PraveenVBongale <praveen.bongale98@gmail.com>
2022-03-01 13:38:59 +0530

    Sealed Class and Unboxing Operation

---
+++ SQFTofHOUSE/SQFT.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class SQFT
+{
+    public static void Main()
+    {
+        int length, width, area;
+        Console.Write("Enter length of room in feet: ");
+        length = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter width of room in feet:");
+        width = Convert.ToInt32(Console.ReadLine());
+        area = length * width;
+        Console.WriteLine("Floor is " + area + " square feet.");
+        Console.ReadLine();
+    }
+}
